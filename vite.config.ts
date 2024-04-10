be8934d917e994a77d39f7612197c660de12a604
Sky <nebulass@naver.com>
2024-04-10 20:36:40 +0900

    leva 숨기기

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 환경 변수에서 BASE_URL을 읽어와 설정합니다.
 export default defineConfig({
   plugins: [react()],
   base: "/",
