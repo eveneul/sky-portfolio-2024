d066325e7ea874fa51811b751d99d3651941c6f0
Sky <nebulass@naver.com>
2024-04-10 18:51:39 +0900

    조건적 base 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
+  base: isProduction ? "/sky-portfolio-2024/" : "/",
   plugins: [react()],
-  base: "/sky-portfolio-2024/",
 });
